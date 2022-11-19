d08e8e51c310264bae6e652b92b73ecc0f2ca2eb
Muhammad Asim <quickbooks2018@gmail.com>
2022-11-19 11:01:10 +0500

    Shift to k8

---
+++ REAME.md
@@ -0,0 +1,56 @@
+
+# Helmify
+
+- Converts k8 yaml files into helm charts
+
+- https://github.com/arttor/helmify
+
+- htps://cloudgeeks.ca
+
+- https://hub.docker.com/r/homebrew/brew
+
+
+- Helmify installation
+
+- Windows Linux MAC
+
+```helmify
+docker network create home --attachable
+
+docker run --name helmify --network home -w /mnt -v "c:/users/Muhammad Asim/Desktop/helmify:/mnt" -id homebrew/brew:latest
+```
+
+- Helmify commands From Directory
+```helmify
+export DIR='k8'
+awk 'FNR==1 && NR!=1  {print "---"}{print}' ${DIR}/*.yaml | helmify -vv cloudgeeks-helm-chart 
+```
+
+- Helmify commands From file
+```helmify
+cat my-app.yaml | helmify -vv cloudgeeks-helmchart
+```
+
+# Kompose
+
+- Convert docker-compose in to kubernetes yaml files
+
+- https://kubernetes.io/docs/tasks/configure-pod-container/translate-compose-kubernetes/
+
+- https://github.com/kubernetes/kompose
+
+- https://github.com/kubernetes/kompose/blob/master/docs/installation.md#docker
+
+- https://github.com/kubernetes/kompose/releases
+
+- Examples https://github.com/kubernetes/kompose/tree/master/examples
+
+```kompose
+docker exec -u root -it helmify bash
+apt update -y && apt install -y sudo
+curl -L https://github.com/kubernetes/kompose/releases/download/v1.27.0/kompose-linux-amd64 -o kompose
+chmod +x kompose
+sudo mv ./kompose /usr/local/bin/kompose
+kompose convert --help
+kompose convert --with-kompose-annotation=false -f docker-compose.yaml
+```
---
+++ k8/kind-cluster.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# Purpose: Kubernetes Cluster
+
+
+######################################
+# Docker & Docker Compose Installation
+######################################
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+rm -f get-docker.sh
+
+
+#######################
+# Kubectl Installation
+#######################
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+####################
+# Helm Installation
+####################
+# https://helm.sh/docs/intro/install/
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+cp /usr/local/bin/helm /usr/bin/helm
+rm -f get_helm.sh
+helm version
+
+###################
+# Kind Installation
+###################
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.0/kind-linux-amd64
+
+# Latest Version
+# https://github.com/kubernetes-sigs/kind
+# curl -Lo ./kind "https://kind.sigs.k8s.io/dl/v0.9.0/kind-$(uname)-amd64"
+# curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.9.0/kind-linux-amd64
+# curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.8.1/kind-linux-amd64
+chmod +x ./kind
+
+mv ./kind /usr/local/bin
+
+#############
+# Kind Config
+#############
+cat << EOF > kind-config.yaml
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+networking:
+  apiServerAddress: 0.0.0.0
+  apiServerPort: 8443
+EOF
+  
+kind create --name cloudgeeks cluster --config kind-config.yaml --image kindest/node:v1.21.10
+  
+  # ssh -N -L 8443:0.0.0.0:8443 cloud_user@d8d0041c.mylabserver.com
+  
+  # export KUBECONFIG=".kube/config"
+  
+  # port forwarding ---> ssh -N -L 8090:0.0.0.0:8090 cloud_user@d8d0041c.mylabserver.com
+  
+  #End
+  
